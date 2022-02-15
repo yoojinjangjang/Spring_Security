1253321f84406725c3adb0f70334422ee343c79b
yoojin Jang <yoojinjangjang@gmail.com>
2022-02-15 23:38:12 +0900

    first commit

---
+++ src/main/java/io/security/spring_security/SecurityConfig.java
@@ -0,0 +1,6 @@
+package io.security.spring_security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter { //사용자 정의 보안기능 구현을 위해 상속을 받는다. 
+}
---
+++ src/main/java/io/security/spring_security/SecurityController.java
@@ -0,0 +1,12 @@
+package io.security.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController{
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+}
